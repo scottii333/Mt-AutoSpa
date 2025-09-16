19b24d6a29586c9cea2db568fdca4704020ccda5
Scottiieee <splendidhoodies@gmail.com>
2025-09-16 11:00:58 +0800

    feat: hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,100 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { CalendarCheck2, PhoneIncoming, CircleCheckBig } from "lucide-react";
 import Image from "next/image";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main>
+      <section className="bg-[#640d14] h-auto flex flex-col sm:flex-row">
+        <div className="w-full sm:w-[50%] text-[#fffcf2] p-[1.5rem] flex flex-col gap-[1rem]">
+          <p
+            className="text-[8px] text-white border border-white/20 rounded-md 
+             text-center px-2 py-1
+             bg-white/10 backdrop-blur-md shadow-md"
+          >
+            COMPLETE AUTO CARE • DETAILING • PROTECTION • SINCE 2011
+          </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <h1 className="font-bold text-[1.5rem]">
+            Complete Auto Care, Right Here in Caloocan
+          </h1>
+
+          <p className="text-[10px] font-semibold">
+            15+ Years • 200,000+ Cars • Trusted by Car Owners
+          </p>
+
+          <p className="text-[10px] ">
+            From quick washes to advanced ceramic coatings, we do it right the
+            first time. Quality service, expert care, and your car back on
+            schedule.
+          </p>
+
+          <Button
+            variant="ghost"
+            className="bg-[#fffcf2] text-[#880d1e] font-bold text-[0.7rem] h-8 cursor-pointer 
+             transition-all duration-300 ease-in-out
+             hover:bg-gradient-to-r hover:from-[#fffcf2] hover:to-[#880d1e] hover:text-white 
+             hover:scale-105"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <CalendarCheck2 size={20} /> Fix My Car
+          </Button>
+
+          <Button
+            variant="ghost"
+            className="bg-[#fffcf2] text-[#880d1e] font-bold text-[0.7rem] h-8 cursor-pointer 
+             transition-all duration-300 ease-in-out
+             hover:bg-gradient-to-r hover:from-[#fffcf2] hover:to-[#880d1e] hover:text-white 
+             hover:scale-105 animate-shake-pause"
           >
-            Read our docs
-          </a>
+            <PhoneIncoming size={20} /> Call Now
+          </Button>
+
+          <p
+            className="text-[0.8rem] text-white border border-white/20 rounded-md 
+             text-center px-2 py-1
+             bg-white/10 backdrop-blur-md shadow-md"
+          >
+            201,567+ <br /> Cars Serviced
+          </p>
+
+          <div className="bg-[#fffcf2] rounded-md pl-[1rem] text-black flex items-center gap-2 justify-center">
+            <div className="relative h-[2rem] w-[2rem]">
+              <Image
+                src="/google-icon.jpg"
+                alt="Logo"
+                fill
+                className="object-contain"
+              />
+            </div>
+            <p className="font-semibold text-[10px]">Google </p>
+            <p className="text-[12px] text-yellow-300">★★★★★</p>
+            <p className="text-[10px] font-semibold">4.9(1,524 Reviews)</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="w-full sm:w-[50%] h-[70dvh] sm:h-screen items-center flex justify-center p-[1rem] ">
+          <div className="relative h-[100%] sm:h-[80%] w-full flex items-center justify-center">
+            <Image
+              src="/hero-image.jpg"
+              alt="Hero Image"
+              fill
+              className="object-cover rounded-md"
+            />
+            <div className="absolute -top-3 -right-2 bg-[#fffcf2] w-[10rem] p-1 rounded-md flex items-center gap-2 justify-center shadow-lg ">
+              <CircleCheckBig size={20} className="text-green-400" />
+              <p className="text-[0.8rem] font-semibold">Certified Detailer</p>
+            </div>
+
+            <div className="absolute bg-white/10 backdrop-blur-[0.3rem] shadow-lg rounded-lg w-[90%] bottom-4 h-[50%] p-[1rem] flex flex-col gap-3">
+              <p className="text-white font-semibold">Get Your Free Service Quote</p>
+             <Input placeholder="Your Name" className="bg-white text-gray-900" />
+<Input placeholder="Email" className="bg-white text-gray-900" />
+              <Button >Get a Quote</Button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
