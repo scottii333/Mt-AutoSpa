b86be5c3e9f3c2f15d4a0a6fdec82d91c1d88672
Scottiieee <splendidhoodies@gmail.com>
2025-09-17 19:18:27 +0800

    feat: use google poltawskiNowi font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poltawski_Nowy } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// Load Poltawski Nowy
+const poltawskiNowy = Poltawski_Nowy({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  style: ["normal", "italic"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -27,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased  min-h-[100rem] flex flex-col`}
+        className={`${poltawskiNowy.className} antialiased min-h-[100rem] flex flex-col`}
       >
         <Header />
         <main className="flex-grow mb-[5rem]">{children}</main>
